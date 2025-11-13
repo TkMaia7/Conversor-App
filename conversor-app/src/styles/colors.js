ff68e62613f9102cd5bf244ebd8e3a6ba31f0b74
ThallesMaia <thallespim@gmail.com>
2025-11-13 08:23:22 -0300

    Projeto Conversor-App

---
+++ conversor-app/src/styles/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#2563eb',
+  secondary: '#03b3bfff',
+  background: '#0f172a',
+  cardBackground: '#1e293b',
+  inputBackground: '#334155',
+  text: '#ffffff',
+  textSecondary: '#94a3b8',
+  disabled: '#334155',
+}; 
+
+//#16a34a
